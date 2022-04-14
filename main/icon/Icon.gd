9aaac37f2e8daa6d1f6a10dc3afbecad40bf63ff
0918nobita <0918nobita@users.noreply.github.com>
2022-04-14 17:42:11 +0900

    Format code

--- main/icon/Icon.gd
+++ main/icon/Icon.gd
@@ -1,18 +1,18 @@
 extends Sprite
 
-var rect: Rect2
+var rect : Rect2
 
 var should_flash := false
-var elapsed: float = 0
+var elapsed : float = 0
 
 func _ready() -> void:
 	var size := get_rect().size * transform.get_scale()
 	rect = Rect2(position - size / 2, size)
 
-func update_flash_modifier(val: float) -> void:
+func update_flash_modifier(val : float) -> void:
 	(material as ShaderMaterial).set_shader_param("flash_modifier", val)
 
-func _process(delta: float) -> void:
+func _process(delta : float) -> void:
 	if should_flash:
 		elapsed += delta
 		update_flash_modifier((1 + sin(2 * elapsed)) / 4)
@@ -20,7 +20,7 @@ func _process(delta: float) -> void:
 		elapsed = 0
 		update_flash_modifier(0)
 
-func _input(event: InputEvent) -> void:
+func _input(event : InputEvent) -> void:
 	if event is InputEventMouseButton:
 		var ev := event as InputEventMouseButton
 		should_flash = rect.has_point(ev.position)
