7ebe2d496369a2fdcb2fe5b1f3c577ccc432c2ba
0918nobita <nobita.0918@gmail.com>
2022-04-15 16:02:03 +0900

    床に関するスクリプトを短縮した

--- main/floor/Floor.gd
+++ main/floor/Floor.gd
@@ -1,10 +1,4 @@
 extends Node2D
 
-func _ready() -> void:
-	pass
-
-func _process(_delta : float) -> void:
-	pass
-
 func _draw() -> void:
 	draw_rect(Rect2(0, 0, 192 * 2, 32 * 2), Color(0, 0, 0, 1))
