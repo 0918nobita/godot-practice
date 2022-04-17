f67ed9ec7b4ab147ea366d571adaf11d0d4b668e
0918nobita <nobita.0918@gmail.com>
2022-04-17 14:11:08 +0900

    クリック判定部分をリファクタリングした

--- main/icon/Icon.gd
+++ main/icon/Icon.gd
@@ -1,15 +1,8 @@
 extends Sprite
 
 
-var rect : Rect2
-
 var should_flash := false
 var elapsed : float = 0
-
-
-func _ready() -> void:
-	var size := get_rect().size * transform.get_scale()
-	rect = Rect2(position - size / 2, size)
 
 
 func update_flash_modifier(val : float) -> void:
@@ -28,8 +21,8 @@ func _process(delta : float) -> void:
 func _input(event : InputEvent) -> void:
 	if event is InputEventMouseButton:
 		var ev := event as InputEventMouseButton
-		should_flash = rect.has_point(ev.position)
+		should_flash = get_rect().has_point(to_local(ev.position))
 
 	elif event as InputEventScreenTouch:
 		var ev := event as InputEventScreenTouch
-		should_flash = rect.has_point(ev.position)
+		should_flash = get_rect().has_point(to_local(ev.position))
