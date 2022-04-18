2f4c88b0f411dee52b771335b492fc46bb6d4d64
0918nobita <nobita.0918@gmail.com>
2022-04-18 19:18:39 +0900

    カウンタの状態管理を Main.gd で行うようにした

--- main/Main.gd
+++ main/Main.gd
@@ -1,5 +1,28 @@
 extends Node2D
 
 
+onready var counter := $Counter as Label
+
+
+var count := 0 setget set_count
+
+
+func set_count(val : int) -> void:
+	counter.text = str(val)
+	count = val
+
+
+func _ready() -> void:
+	set_count(0)
+
+
+func _on_DecrementButton_button_up():
+	set_count(count - 1)
+
+
+func _on_IncrementButton_button_up():
+	set_count(count + 1)
+
+
 func _on_QuitButton_button_up():
 	get_tree().quit()
